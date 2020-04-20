2a9934b8b462c117d30e946c4412af8d4de4f3b5
R <roksana_sobczak@wp.pl>
2020-04-20 11:44:00 +0200

    add files

---
+++ src/components/HomeHeader.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+//import {Link} from "react-router-dom";
+
+
+
+const HomeHeader  = () => {
+
+    return(
+        <header className = "header-container">
+            <div className = "header-photo">
+                lalalalla
+            </div>
+        </header>
+    )
+
+
+}
+
+export default HomeHeader;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import HomeHeader from '../components/HomeHeader';
+
+
+
+class Home extends Component{
+    render(){
+        return(
+            <>
+                <HomeHeader />
+            </>
+        );
+    }
+}
+export default Home;
