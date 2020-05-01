ee12efcb6b39472870826a835289b6934bb392b1
R <roksana_sobczak@wp.pl>
2020-05-01 17:36:45 +0200

    nav styles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-//import {Link} from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
 import MenuIcon from "../assets/menu.png";
 import Row from 'react-bootstrap/Row';
